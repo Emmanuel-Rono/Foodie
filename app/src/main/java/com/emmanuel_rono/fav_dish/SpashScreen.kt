d64e9679bbdd80b997d0e333466c96e5a5a7a908
Emmanuel-Rono <ronoemmanuel933@gmail.com>
2023-06-22 13:08:51 +0300

    Working with the SlashScreen

--- app/src/main/java/com/emmanuel_rono/fav_dish/SpashScreen.kt
+++ app/src/main/java/com/emmanuel_rono/fav_dish/SpashScreen.kt
@@ -1,8 +1,16 @@
 package com.emmanuel_rono.fav_dish
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
+import android.os.Handler
+import android.os.Looper
+import android.view.Window
+import android.view.WindowInsets
+import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import com.emmanuel_rono.fav_dish.databinding.ActivitySpashScreenBinding
 
 class SpashScreen : AppCompatActivity() {
@@ -11,5 +19,32 @@ class SpashScreen : AppCompatActivity() {
         //create binding view
         val splashBinding:ActivitySpashScreenBinding=ActivitySpashScreenBinding.inflate(layoutInflater)
         setContentView(splashBinding.root)
-}
-}
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R)
+        {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        }
+        else
+        {
+            @Suppress("DEPRECIATION")
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAGS_CHANGED
+            )
+        }
+    val splashScreen=AnimationUtils.loadAnimation(this,R.anim.animation_splash)
+    splashBinding.splashext.animation=splashScreen
+        splashScreen.setAnimationListener(object :Animation.AnimationListener{
+           override fun onAnimationStart(p0: Animation?) {
+               /*/TODO("Not yet implemented")*/
+            }
+            override fun onAnimationEnd(p0: Animation?) {
+               Handler(Looper.getMainLooper()).postDelayed({startActivity(Intent(this@SpashScreen,
+                   MainActivity::class.java))
+                   finish()},
+               1000)
+            }
+            override fun onAnimationRepeat(p0: Animation?) {
+                TODO("Not yet implemented")
+            }
+        })
+}}
