4d84c56bc750ee9069838df7997efacac905f346
Emmanuel-Rono <ronoemmanuel933@gmail.com>
2023-06-27 10:38:03 +0300

    Working with ToolBar

--- app/src/main/java/com/emmanuel_rono/fav_dish/Presentation/Activities/AddUpdateDish.kt
+++ app/src/main/java/com/emmanuel_rono/fav_dish/Presentation/Activities/AddUpdateDish.kt
@@ -3,10 +3,26 @@ package com.emmanuel_rono.fav_dish.Presentation.Activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.emmanuel_rono.fav_dish.R
+import com.emmanuel_rono.fav_dish.databinding.ActivityAddUpdateDishBinding
 
 class AddUpdateDish : AppCompatActivity() {
+     private lateinit var updateDishBinding:ActivityAddUpdateDishBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_update_dish)
+        updateDishBinding=ActivityAddUpdateDishBinding.inflate(layoutInflater)
+        setContentView(updateDishBinding.root)
+        setupActionbar()
+
     }
-}
+//Setting the Action Bar
+    //create a function to do that
+    private fun setupActionbar()
+{
+        setSupportActionBar(updateDishBinding.toolbar)
+    supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    //Set onclick Listerner
+    updateDishBinding.toolbar.setNavigationOnClickListener{
+    onBackPressed()
+
+    }
+}}
