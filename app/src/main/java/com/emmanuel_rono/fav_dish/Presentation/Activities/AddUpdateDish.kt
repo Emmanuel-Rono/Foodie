3acc8d74579c5c8aa661fd24b12fb0802ada62c4
Emmanuel-Rono <ronoemmanuel933@gmail.com>
2023-06-27 11:04:02 +0300

    Proceeding

--- app/src/main/java/com/emmanuel_rono/fav_dish/Presentation/Activities/AddUpdateDish.kt
+++ app/src/main/java/com/emmanuel_rono/fav_dish/Presentation/Activities/AddUpdateDish.kt
@@ -1,19 +1,25 @@
 package com.emmanuel_rono.fav_dish.Presentation.Activities
 
+import android.icu.lang.UCharacter.GraphemeClusterBreak.V
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import com.emmanuel_rono.fav_dish.R
 import com.emmanuel_rono.fav_dish.databinding.ActivityAddUpdateDishBinding
 
-class AddUpdateDish : AppCompatActivity() {
+class AddUpdateDish : AppCompatActivity() , View.OnClickListener{
      private lateinit var updateDishBinding:ActivityAddUpdateDishBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         updateDishBinding=ActivityAddUpdateDishBinding.inflate(layoutInflater)
         setContentView(updateDishBinding.root)
         setupActionbar()
-
+        updateDishBinding.dishImage.setOnClickListener(this)
     }
+
 //Setting the Action Bar
     //create a function to do that
     private fun setupActionbar()
@@ -25,4 +31,18 @@ class AddUpdateDish : AppCompatActivity() {
     onBackPressed()
 
     }
-}}
+
+}
+    @RequiresApi(Build.VERSION_CODES.N)
+    override fun onClick(p0: View?) {
+        if (p0 != null) {
+            when(p0.id){
+                R.id.add_dish_image -> {
+                    Toast.makeText(this,"Cmaera Clicked",Toast.LENGTH_SHORT).show()
+                    return
+                }
+
+            }
+        }
+    }
+}
