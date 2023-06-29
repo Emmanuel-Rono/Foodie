12e82e9120bf375499f8bdeb85b91292df12df77
Emmanuel-Rono <ronoemmanuel933@gmail.com>
2023-06-29 10:39:06 +0300

    Working with AddUpdateDish

--- app/src/main/java/com/emmanuel_rono/fav_dish/Presentation/Activities/AddUpdateDish.kt
+++ app/src/main/java/com/emmanuel_rono/fav_dish/Presentation/Activities/AddUpdateDish.kt
@@ -15,6 +15,7 @@ import android.view.View
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.emmanuel_rono.fav_dish.R
 import com.emmanuel_rono.fav_dish.databinding.ActivityAddUpdateDishBinding
 import com.emmanuel_rono.fav_dish.databinding.PopupScreenAddDishBinding
@@ -27,128 +28,165 @@ import com.karumi.dexter.listener.PermissionRequest
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import com.karumi.dexter.listener.single.PermissionListener
 
+class AddUpdateDish : AppCompatActivity(), View.OnClickListener {
 
-class AddUpdateDish : AppCompatActivity() , View.OnClickListener{
-     private lateinit var updateDishBinding:ActivityAddUpdateDishBinding
+    private lateinit var updateDishBinding: ActivityAddUpdateDishBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        updateDishBinding=ActivityAddUpdateDishBinding.inflate(layoutInflater)
+        updateDishBinding = ActivityAddUpdateDishBinding.inflate(layoutInflater)
         setContentView(updateDishBinding.root)
-        setupActionbar()
+        setupActionBar()
         updateDishBinding.addDishImage.setOnClickListener(this)
-    }
-//Setting the Action Bar
-    //create a function to do that
-    private fun setupActionbar()
-{
-        setSupportActionBar(updateDishBinding.toolbar)
-    supportActionBar?.setDisplayHomeAsUpEnabled(true)
-    //Set onclick Listerner
-    updateDishBinding.toolbar.setNavigationOnClickListener{
-    onBackPressed()
+        updateDishBinding.dishimage.setOnClickListener(this)
     }
 
-}
+    private fun setupActionBar() {
+        setSupportActionBar(updateDishBinding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        updateDishBinding.toolbar.setNavigationOnClickListener {
+            onBackPressed()
+        }
+    }
+
     @RequiresApi(Build.VERSION_CODES.N)
-    override fun onClick(V: View?) {
-        if (V != null) {
-            when(V.id){
-                R.id.add_dish_image-> {
-                    ImageSelectionDialog()
-                    return
-                }
+    override fun onClick(view: View) {
+        when (view.id) {
+            R.id.addDishImage -> {
+                imageSelectionDialog()
             }
         }
     }
-    private fun ImageSelectionDialog()
-    {
-        val dialog=Dialog(this)
-        val binding:PopupScreenAddDishBinding=PopupScreenAddDishBinding.inflate(layoutInflater)
+
+    private fun imageSelectionDialog() {
+        val dialog = Dialog(this)
+        val binding: PopupScreenAddDishBinding = PopupScreenAddDishBinding.inflate(layoutInflater)
         dialog.setContentView(binding.root)
 
-
-        binding.addViaCamera.setOnClickListener{
-            Dexter.withContext( this).withPermissions(
-                Manifest.permission.READ_EXTERNAL_STORAGE,
-                Manifest.permission.CAMERA
-            ).withListener(object :MultiplePermissionsListener
-            {
-                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
-                    report?.let {
-                        if(report.areAllPermissionsGranted())
-                        {
-                            val intent=Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                            startActivityForResult(intent, camra)
-                    }
-
-                    }
-                }
-                override fun onPermissionRationaleShouldBeShown(
-                    Permissions: MutableList<PermissionRequest>?,
-                    Token: PermissionToken?
-                ) {
-                    showAlertDialogForPermissions()
-                }
-
-            }).onSameThread().check()
+        binding.addViaCamera.setOnClickListener {
+            requestCameraPermission()
             dialog.dismiss()
         }
 
-
-        binding.addViaGallery.setOnClickListener{
-           Dexter.withContext( this).withPermission(
-               Manifest.permission.READ_EXTERNAL_STORAGE,
-               ).withListener(object:PermissionListener
-           {
-               override fun onPermissionGranted(p0: PermissionGrantedResponse?) {
-                  Toast.makeText(this@AddUpdateDish, "Got gallery Permission",Toast.LENGTH_LONG).show()
-               }
-               override fun onPermissionDenied(p0: PermissionDeniedResponse?) {
-                   Toast.makeText(this@AddUpdateDish,"Denied Permission",Toast.LENGTH_SHORT).show()
-               }
-               override fun onPermissionRationaleShouldBeShown(
-                   p0: PermissionRequest?,
-                   p1: PermissionToken?
-               ) {
-                  showAlertDialogForPermissions()
-               }
-
-           }).onSameThread().check()
+        binding.addViaGallery.setOnClickListener {
+            requestGalleryPermission()
             dialog.dismiss()
         }
 
         dialog.show()
     }
-    @Deprecated("Deprecated in Java")
-    override fun onActivityResult(requestCode:Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode,data)
-        if(resultCode== camra){
-            data?.let {
-                val thumbnail:Bitmap= data.extras?.get("data") as Bitmap
-                updateDishBinding.dishimage.setImageBitmap(thumbnail)
+
+    private fun requestCameraPermission() {
+        Dexter.withContext(this)
+            .withPermissions(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.CAMERA
+            )
+            .withListener(object : MultiplePermissionsListener {
+                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                    if (report != null && report.areAllPermissionsGranted()) {
+                        startCamera()
+                    } else {
+                        showPermissionDeniedDialog()
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permissions: MutableList<PermissionRequest>?,
+                    token: PermissionToken?
+                ) {
+                    showPermissionDeniedDialog()
+                }
+            }).check()
+    }
+
+    private fun requestGalleryPermission() {
+        Dexter.withContext(this)
+            .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+            .withListener(object : PermissionListener {
+                override fun onPermissionGranted(response: PermissionGrantedResponse?) {
+                    startGallery()
+                    Toast.makeText(
+                        this@AddUpdateDish,
+                        "Got gallery permission",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+
+                override fun onPermissionDenied(response: PermissionDeniedResponse?) {
+                    Toast.makeText(
+                        this@AddUpdateDish,
+                        "Permission denied",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permission: PermissionRequest?,
+                    token: PermissionToken?
+                ) {
+                    showPermissionDeniedDialog()
+                }
+            }).check()
+    }
+
+    private fun startCamera() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(intent, REQUEST_CODE_CAMERA)
+    }
+    private fun startGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(intent, REQUEST_CODE_GALLERY)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            REQUEST_CODE_CAMERA -> {
+                if (resultCode == RESULT_OK) {
+                    val thumbnail: Bitmap? = data?.extras?.get("data") as? Bitmap
+                    thumbnail?.let {
+                        updateDishBinding.dishimage.setImageBitmap(thumbnail)
+                        updateDishBinding.addDishImage.setImageDrawable(
+                            ContextCompat.getDrawable(this, R.drawable.ic_edit_icon)
+                        )
+                    }
+
+                }
             }
+            REQUEST_CODE_GALLERY -> {
+                if (resultCode == RESULT_OK && data != null) {
+                    val selectedImage: Uri? = data.data
+                    val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, selectedImage)
+                    updateDishBinding.dishimage.setImageBitmap(bitmap)
+                    updateDishBinding.addDishImage.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.ic_edit_icon))
+                }
+            }
+
         }
     }
-    private fun showAlertDialogForPermissions()
-    {
-        AlertDialog.Builder(this).setMessage("ooops!, Looks Like you have Turned Off Permission Required for This Action")
-            .setPositiveButton("GO TO SETTINGS")
-            {_,_->
+
+    private fun showPermissionDeniedDialog() {
+        AlertDialog.Builder(this)
+            .setMessage("Oops! Looks like you have turned off the required permissions for this action.")
+            .setPositiveButton("Go to Settings") { _, _ ->
                 try {
-                    val intent= Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
-                    val uri= Uri.fromParts("package",packageName,null)
-                    intent.data=uri
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
                     startActivity(intent)
-                }catch (e:ActivityNotFoundException)
-                {
+                } catch (e: ActivityNotFoundException) {
                     e.printStackTrace()
                 }
             }
-            .setNegativeButton("cancel"){dialog,_ ->
-                dialog.dismiss()}.show()
-    }
-    companion object{
-        private const val camra=1
+            .setNegativeButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }
+            .show()
     }
 
+    companion object {
+        private const val REQUEST_CODE_CAMERA = 1
+        private const val REQUEST_CODE_GALLERY=2
+    }
 }
