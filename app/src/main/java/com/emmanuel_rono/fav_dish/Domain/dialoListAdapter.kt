3db29810309b9f2fa5214b41fa95cde86f7e0af7
Emmanuel-Rono <ronoemmanuel933@gmail.com>
2023-06-30 11:33:43 +0300

    Proceeding

---
+++ app/src/main/java/com/emmanuel_rono/fav_dish/Domain/dialoListAdapter.kt
@@ -0,0 +1,41 @@
+package com.emmanuel_rono.fav_dish.Domain
+
+import android.app.Activity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Adapter
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Recycler
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.emmanuel_rono.fav_dish.databinding.DialoglistBinding
+
+class dialoListAdapter(
+    private val activity:Activity,
+    private val listItems:List<String>,
+    private val selection:String
+): RecyclerView.Adapter<dialoListAdapter.dialogListViwHOlder>()
+{
+
+
+    inner class dialogListViwHOlder(View:DialoglistBinding) :ViewHolder(View.root)
+    {
+        val tvtext=View.titleTv
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): dialogListViwHOlder {
+val binding:DialoglistBinding=DialoglistBinding.inflate(LayoutInflater.from(activity),parent,false)
+        return dialogListViwHOlder(binding)
+    }
+
+    override fun onBindViewHolder(holder: dialogListViwHOlder, position: Int) {
+val item=listItems[position]
+        holder.tvtext.text=item
+
+    }
+
+    override fun getItemCount(): Int {
+        return listItems.size
+    }
+}
